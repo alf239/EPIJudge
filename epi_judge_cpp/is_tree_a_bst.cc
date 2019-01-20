63761156c8c710238c6ea28c6fb2f2e53a311a12
Alexey Filippov <afilippov@acm.org>
2019-01-20 16:01:39 +0000

    14.1 BST property

--- epi_judge_cpp/is_tree_a_bst.cc
+++ epi_judge_cpp/is_tree_a_bst.cc
@@ -1,16 +1,38 @@
 #include <memory>
+#include <algorithm>
+#include <limits>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
+struct bst_property {
+	bool ok, empty;
+	int min, max;
+};
+
+bst_property check_bst_property(const unique_ptr<BinaryTreeNode<int>>& tree) {
+	if (tree == nullptr) return { true, true, std::numeric_limits<int>::max(), std::numeric_limits<int>::min() };
+	
+	auto l = check_bst_property(tree->left);
+	auto r = check_bst_property(tree->right);
+
+	int x = tree->data;
+
+	return {
+		l.ok && r.ok && (l.empty || x >= l.max) && (r.empty || x <= r.min),
+		false,
+		std::min(x, std::min(r.min, l.min)),
+		std::max(x, std::max(r.max, l.max))
+	};
+}
+
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+	return check_bst_property(tree).ok;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree"};
-  return GenericTestMain(args, "is_tree_a_bst.cc", "is_tree_a_bst.tsv",
-                         &IsBinaryTreeBST, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "tree" };
+	return GenericTestMain(args, "is_tree_a_bst.cc", "is_tree_a_bst.tsv",
+		&IsBinaryTreeBST, DefaultComparator{}, param_names);
 }
