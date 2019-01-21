5a6ffc617f048ed6bf2e769b3beea1adc37badb4
Alexey Filippov <afilippov@acm.org>
2019-01-21 08:11:29 +0000

    17.1 A (slow) threesum

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -1,15 +1,26 @@
 #include <vector>
+#include <unordered_set>>
+#include <algorithm>
 #include "test_framework/generic_test.h"
 using std::vector;
 
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+	std::unordered_set<int> pairs;
+	for (int i = 0; i < A.size(); i++) {
+		int a = A[i];
+		for (int j = 0; j <= i; j++) {
+			pairs.emplace(A[j] + a);
+		}
+		if (a * 3 == t || pairs.count(t - a)) {
+			return true;
+		}
+	}
+	return false;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "t"};
-  return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "A", "t" };
+	return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
+		DefaultComparator{}, param_names);
 }
