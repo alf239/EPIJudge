528400485578bdaeaca73cf761a317a02a0fcc7b
Alexey Filippov <afilippov@acm.org>
2019-02-03 22:57:27 +0000

    14.3 K largest in BST

--- epi_judge_cpp/k_largest_values_in_bst.cc
+++ epi_judge_cpp/k_largest_values_in_bst.cc
@@ -5,15 +5,26 @@
 using std::unique_ptr;
 using std::vector;
 
+void CollectKLargestInBST(vector<int>* acc, BstNode<int>* tree, int *k) {
+	if (!tree) return;
+	CollectKLargestInBST(acc, tree->right.get(), k);
+	if (*k == 0) return;
+	acc->push_back(tree->data);
+	(*k)--;
+	if (*k == 0) return;
+	CollectKLargestInBST(acc, tree->left.get(), k);
+}
+
 vector<int> FindKLargestInBST(const unique_ptr<BstNode<int>>& tree, int k) {
-  // TODO - you fill in here.
-  return {};
+	vector<int> result;
+	CollectKLargestInBST(&result, tree.get(), &k);
+	return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "k"};
-  return GenericTestMain(args, "k_largest_values_in_bst.cc",
-                         "k_largest_values_in_bst.tsv", &FindKLargestInBST,
-                         &UnorderedComparator<std::vector<int>>, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "tree", "k" };
+	return GenericTestMain(args, "k_largest_values_in_bst.cc",
+		"k_largest_values_in_bst.tsv", &FindKLargestInBST,
+		&UnorderedComparator<std::vector<int>>, param_names);
 }
