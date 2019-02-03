da01ec1048cf7bb85ee6d88dd90dfa64560c8e3b
Alexey Filippov <afilippov@acm.org>
2019-02-03 22:51:06 +0000

    14.2 First greater than in BST

--- epi_judge_cpp/search_first_greater_value_in_bst.cc
+++ epi_judge_cpp/search_first_greater_value_in_bst.cc
@@ -3,21 +3,27 @@
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
-BstNode<int>* FindFirstGreaterThanK(const unique_ptr<BstNode<int>>& tree,
-                                    int k) {
-  // TODO - you fill in here.
-  return nullptr;
+BstNode<int>* FindFirstGreaterThanK(BstNode<int>* current, BstNode<int>* bigger, int k) {
+	if (!current) return bigger;
+	if (current->data <= k) return FindFirstGreaterThanK(current->right.get(), bigger, k);
+	return FindFirstGreaterThanK(current->left.get(), current, k);
 }
+
+BstNode<int>* FindFirstGreaterThanK(const unique_ptr<BstNode<int>>& tree,
+	int k) {
+	return FindFirstGreaterThanK(tree.get(), nullptr, k);
+}
+
 int FindFirstGreaterThanKWrapper(const unique_ptr<BstNode<int>>& tree, int k) {
-  auto result = FindFirstGreaterThanK(tree, k);
-  return result ? result->data : -1;
+	auto result = FindFirstGreaterThanK(tree, k);
+	return result ? result->data : -1;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree", "k"};
-  return GenericTestMain(args, "search_first_greater_value_in_bst.cc",
-                         "search_first_greater_value_in_bst.tsv",
-                         &FindFirstGreaterThanKWrapper, DefaultComparator{},
-                         param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "tree", "k" };
+	return GenericTestMain(args, "search_first_greater_value_in_bst.cc",
+		"search_first_greater_value_in_bst.tsv",
+		&FindFirstGreaterThanKWrapper, DefaultComparator{},
+		param_names);
 }
