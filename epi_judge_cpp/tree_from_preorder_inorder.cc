f271b0efa0a36e98ea9e23567b9cca1f2c4223d1
Alexey Filippov <afilippov@acm.org>
2019-02-07 19:04:00 +0000

    9.11 Preorder/inorder reconstruction

--- epi_judge_cpp/tree_from_preorder_inorder.cc
+++ epi_judge_cpp/tree_from_preorder_inorder.cc
@@ -5,15 +5,26 @@
 using std::vector;
 
 unique_ptr<BinaryTreeNode<int>> BinaryTreeFromPreorderInorder(
-    const vector<int>& preorder, const vector<int>& inorder) {
-  // TODO - you fill in here.
-  return nullptr;
+	const vector<int>& preorder, const vector<int>& inorder) {
+	if (preorder.empty()) return nullptr;
+
+	int pivot = preorder[0];
+	int i = 0;
+	while (inorder[i] != pivot) i++;
+
+	auto &left = BinaryTreeFromPreorderInorder(
+		{ preorder.cbegin() + 1, preorder.cbegin() + i + 1},
+		{ inorder.cbegin(), inorder.cbegin() + i });
+	auto &right = BinaryTreeFromPreorderInorder(
+		{ preorder.cbegin() + i + 1, preorder.cend() },
+		{ inorder.cbegin() + i + 1, inorder.cend() });
+	return std::make_unique<BinaryTreeNode<int>>(pivot, std::move(left), std::move(right));
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"preorder", "inorder"};
-  return GenericTestMain(
-      args, "tree_from_preorder_inorder.cc", "tree_from_preorder_inorder.tsv",
-      &BinaryTreeFromPreorderInorder, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "preorder", "inorder" };
+	return GenericTestMain(
+		args, "tree_from_preorder_inorder.cc", "tree_from_preorder_inorder.tsv",
+		&BinaryTreeFromPreorderInorder, DefaultComparator{}, param_names);
 }
