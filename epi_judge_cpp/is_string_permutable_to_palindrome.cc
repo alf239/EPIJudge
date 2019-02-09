19f071c00e161c89b3d5d939a953e9d8c7de2193
Alexey Filippov <afilippov@acm.org>
2019-02-09 00:47:57 +0000

    12.1 Palindromizable string

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,16 +1,24 @@
 #include <string>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 using std::string;
 
 bool CanFormPalindrome(const string& s) {
-  // TODO - you fill in here.
-  return true;
+	std::unordered_map<char, int> counters;
+	for (const char &c : s) {
+		counters[c]++;
+	}
+	int odd = 0;
+	for (const auto &entry : counters) {
+		if (entry.second % 2) odd++;
+	}
+	return odd < 2;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "is_string_permutable_to_palindrome.cc",
-                         "is_string_permutable_to_palindrome.tsv",
-                         &CanFormPalindrome, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "s" };
+	return GenericTestMain(args, "is_string_permutable_to_palindrome.cc",
+		"is_string_permutable_to_palindrome.tsv",
+		&CanFormPalindrome, DefaultComparator{}, param_names);
 }
