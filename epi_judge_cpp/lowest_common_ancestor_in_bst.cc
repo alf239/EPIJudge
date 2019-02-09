2d98c4a7f16e72f325536eae5803734980acbe1d
Alexey Filippov <afilippov@acm.org>
2019-02-09 11:18:26 +0000

    14.4 Lowest common ancestor in BST

--- epi_judge_cpp/lowest_common_ancestor_in_bst.cc
+++ epi_judge_cpp/lowest_common_ancestor_in_bst.cc
@@ -6,31 +6,36 @@
 #include "test_framework/timed_executor.h"
 using std::unique_ptr;
 
-// Input nodes are nonempty and the key at s is less than or equal to that at
-// b.
+// Input nodes are nonempty and the key at s is less than or equal to that at b.
 BstNode<int>* FindLCA(const unique_ptr<BstNode<int>>& tree,
-                      const unique_ptr<BstNode<int>>& s,
-                      const unique_ptr<BstNode<int>>& b) {
-  // TODO - you fill in here.
-  return nullptr;
+	const unique_ptr<BstNode<int>>& s,
+	const unique_ptr<BstNode<int>>& b) {
+	if (tree->data == s->data || tree->data == b->data) return tree.get();
+
+	if (tree->data > s->data) {
+		if (tree->data > b->data)
+			return FindLCA(tree->left, s, b);
+		else
+			return tree.get();
+	} return FindLCA(tree->right, s, b);
 }
 int LcaWrapper(TimedExecutor& executor,
-               const std::unique_ptr<BstNode<int>>& tree, int key0, int key1) {
-  const unique_ptr<BstNode<int>>& node0 = MustFindNode(tree, key0);
-  const unique_ptr<BstNode<int>>& node1 = MustFindNode(tree, key1);
+	const std::unique_ptr<BstNode<int>>& tree, int key0, int key1) {
+	const unique_ptr<BstNode<int>>& node0 = MustFindNode(tree, key0);
+	const unique_ptr<BstNode<int>>& node1 = MustFindNode(tree, key1);
 
-  auto result = executor.Run([&] { return FindLCA(tree, node0, node1); });
+	auto result = executor.Run([&] { return FindLCA(tree, node0, node1); });
 
-  if (!result) {
-    throw TestFailure("Result can not be nullptr");
-  }
-  return result->data;
+	if (!result) {
+		throw TestFailure("Result can not be nullptr");
+	}
+	return result->data;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "tree", "key0", "key1"};
-  return GenericTestMain(args, "lowest_common_ancestor_in_bst.cc",
-                         "lowest_common_ancestor_in_bst.tsv", &LcaWrapper,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "tree", "key0", "key1" };
+	return GenericTestMain(args, "lowest_common_ancestor_in_bst.cc",
+		"lowest_common_ancestor_in_bst.tsv", &LcaWrapper,
+		DefaultComparator{}, param_names);
 }
