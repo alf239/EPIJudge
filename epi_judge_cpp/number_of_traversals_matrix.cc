edd47201a219abea024497a4c9babe10cc20b89a
Alexey Filippov <afilippov@acm.org>
2019-02-10 14:41:53 +0000

    16.3 Number of ways (DP)

--- epi_judge_cpp/number_of_traversals_matrix.cc
+++ epi_judge_cpp/number_of_traversals_matrix.cc
@@ -1,14 +1,22 @@
+#include <vector>
 #include "test_framework/generic_test.h"
 
+using std::vector;
+
 int NumberOfWays(int n, int m) {
-  // TODO - you fill in here.
-  return 0;
+	vector<int> dp(n, 1);
+	for (int i = 1; i < m; i++) {
+		for (int j = 1; j < n; j++) {
+			dp[j] += dp[j - 1];
+		}
+	}
+	return dp[n - 1];
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"n", "m"};
-  return GenericTestMain(args, "number_of_traversals_matrix.cc",
-                         "number_of_traversals_matrix.tsv", &NumberOfWays,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "n", "m" };
+	return GenericTestMain(args, "number_of_traversals_matrix.cc",
+		"number_of_traversals_matrix.tsv", &NumberOfWays,
+		DefaultComparator{}, param_names);
 }
