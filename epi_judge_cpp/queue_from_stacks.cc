ea3198e57c3066cc435e285fd99d5c528cff64f5
Alexey Filippov <afilippov@acm.org>
2019-02-14 19:17:28 +0000

    8.8 Okasaki queue

--- epi_judge_cpp/queue_from_stacks.cc
+++ epi_judge_cpp/queue_from_stacks.cc
@@ -1,36 +1,49 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <stack>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_failure.h"
 using std::length_error;
 class Queue {
- public:
-  void Enqueue(int x) {
-    // TODO - you fill in here.
-    return;
-  }
-  int Dequeue() {
-    // TODO - you fill in here.
-    return 0;
-  }
+private:
+	std::stack<int> front, rear;
+
+public:
+	void Enqueue(int x) {
+		rear.push(x);
+	}
+	int Dequeue() {
+		if (front.empty()) {
+			while (!rear.empty()) {
+				front.push(rear.top());
+				rear.pop();
+			}
+		}
+		int res = front.top();
+		front.pop();
+		return res;
+	}
 };
 struct QueueOp {
-  enum { kConstruct, kDequeue, kEnqueue } op;
-  int argument;
+	enum { kConstruct, kDequeue, kEnqueue } op;
+	int argument;
 
-  QueueOp(const std::string& op_string, int arg) : argument(arg) {
-    if (op_string == "Queue") {
-      op = kConstruct;
-    } else if (op_string == "dequeue") {
-      op = kDequeue;
-    } else if (op_string == "enqueue") {
-      op = kEnqueue;
-    } else {
-      throw std::runtime_error("Unsupported queue operation: " + op_string);
-    }
-  }
+	QueueOp(const std::string& op_string, int arg) : argument(arg) {
+		if (op_string == "Queue") {
+			op = kConstruct;
+		}
+		else if (op_string == "dequeue") {
+			op = kDequeue;
+		}
+		else if (op_string == "enqueue") {
+			op = kEnqueue;
+		}
+		else {
+			throw std::runtime_error("Unsupported queue operation: " + op_string);
+		}
+	}
 };
 
 template <>
@@ -38,33 +51,34 @@ struct SerializationTraits<QueueOp> : UserSerTraits<QueueOp, std::string, int> {
 };
 
 void QueueTester(const std::vector<QueueOp>& ops) {
-  try {
-    Queue q;
-    for (auto& x : ops) {
-      switch (x.op) {
-        case QueueOp::kConstruct:
-          break;
-        case QueueOp::kDequeue: {
-          int result = q.Dequeue();
-          if (result != x.argument) {
-            throw TestFailure("Dequeue: expected " +
-                              std::to_string(x.argument) + ", got " +
-                              std::to_string(result));
-          }
-        } break;
-        case QueueOp::kEnqueue:
-          q.Enqueue(x.argument);
-          break;
-      }
-    }
-  } catch (length_error&) {
-    throw TestFailure("Unexpected length_error exception");
-  }
+	try {
+		Queue q;
+		for (auto& x : ops) {
+			switch (x.op) {
+			case QueueOp::kConstruct:
+				break;
+			case QueueOp::kDequeue: {
+				int result = q.Dequeue();
+				if (result != x.argument) {
+					throw TestFailure("Dequeue: expected " +
+						std::to_string(x.argument) + ", got " +
+						std::to_string(result));
+				}
+			} break;
+			case QueueOp::kEnqueue:
+				q.Enqueue(x.argument);
+				break;
+			}
+		}
+	}
+	catch (length_error&) {
+		throw TestFailure("Unexpected length_error exception");
+	}
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"ops"};
-  return GenericTestMain(args, "queue_from_stacks.cc", "queue_from_stacks.tsv",
-                         &QueueTester, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "ops" };
+	return GenericTestMain(args, "queue_from_stacks.cc", "queue_from_stacks.tsv",
+		&QueueTester, DefaultComparator{}, param_names);
 }
