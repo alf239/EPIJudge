a87ff36d03bea0cea21e4d5c0f8d1efef25e08a2
Alexey Filippov <afilippov@acm.org>
2019-02-15 00:07:33 +0000

    9.10 O(1) space tree traversal

--- epi_judge_cpp/tree_with_parent_inorder.cc
+++ epi_judge_cpp/tree_with_parent_inorder.cc
@@ -1,17 +1,58 @@
 #include <vector>
+#include <functional>
+#include <limits>
 #include "binary_tree_with_parent_prototype.h"
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::pair;
+using std::function;
+
+// 1. left - step left
+// 2. up - consume the node, try right-left
+// 3. once right, and left again
+
+struct State {
+	BinaryTreeNode<int> * prev;
+	BinaryTreeNode<int> * node;
+};
+
+State right_or_up(State state) {
+	if (state.node->right) return { state.node, state.node->right.get() };
+	else return { state.node, state.node->parent };
+}
+
+State traverse(State state, const function<void(int)> &consume) {
+	if (state.prev == state.node->parent) {
+		if (state.node->left) return { state.node, state.node->left.get() };
+		else {
+			consume(state.node->data);
+			return right_or_up(state);
+		}
+	}
+	else if (state.prev == state.node->left.get()) {
+		consume(state.node->data);
+		return right_or_up(state);
+	}
+	else {
+		return { state.node, state.node->parent };
+	}
+}
 
 vector<int> InorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+	vector<int> result;
+
+	State state{ nullptr, tree.get() };
+	while (state.node) {
+		state = traverse(state, [&result](int x) -> void { result.push_back(x); });
+	}
+
+	return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree"};
-  return GenericTestMain(args, "tree_with_parent_inorder.cc",
-                         "tree_with_parent_inorder.tsv", &InorderTraversal,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "tree" };
+	return GenericTestMain(args, "tree_with_parent_inorder.cc",
+		"tree_with_parent_inorder.tsv", &InorderTraversal,
+		DefaultComparator{}, param_names);
 }
