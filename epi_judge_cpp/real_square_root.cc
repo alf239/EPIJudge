b159481aaafb093b85fcb46b0798fea725b6283a
Alexey Filippov <afilippov@acm.org>
2019-02-15 07:47:58 +0000

    11.5 Newton method for square root

--- epi_judge_cpp/real_square_root.cc
+++ epi_judge_cpp/real_square_root.cc
@@ -1,13 +1,28 @@
 #include "test_framework/generic_test.h"
 
-double SquareRoot(double x) {
-  // TODO - you fill in here.
-  return 0.0;
+const double epsilon = 1e-6;
+
+// x = sqrt(a) => x = a / x => guess is halfway betweeen x and a / x
+
+double improve(double a, double x) {
+	return 0.5 * (x + a / x);
+}
+
+double SquareRoot(double a) {
+	if (a == 0.0) return 0.0;
+
+	double guess = a / 2;
+	double prev = 2;
+	while (std::abs(prev - guess) > epsilon) {
+		prev = guess;
+		guess = improve(a, guess);
+	}
+	return guess;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "real_square_root.cc", "real_square_root.tsv",
-                         &SquareRoot, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "x" };
+	return GenericTestMain(args, "real_square_root.cc", "real_square_root.tsv",
+		&SquareRoot, DefaultComparator{}, param_names);
 }
