65b3d5a71fa49bbb8b8f845989c979cba7077b71
Alexey Filippov <afilippov@acm.org>
2019-03-25 21:41:23 +0000

    17.7 Max trapped water 2 - in O(n)

--- epi_judge_cpp/max_trapped_water.cc
+++ epi_judge_cpp/max_trapped_water.cc
@@ -1,19 +1,28 @@
 #include <vector>
+#include <algorithm>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::min;
 
 int GetMaxTrappedWater(const vector<int>& heights) {
-	int mx_h = 0;
-	for (auto &h : heights) {
-		if (mx_h < h) mx_h = h;
-	}
+	int l = 0, r = heights.size() - 1;
 	int mx_t = 0;
-	for (int h = 1; h <= mx_h; h++) {
-		int l = 0, r = heights.size() - 1;
-		while (heights[l] < h) l++;
-		while (heights[r] < h) r--;
-		const int t = (r - l) * h;
+	while (r > l) {
+		int t = (r - l) * min(heights[l], heights[r]);
 		if (mx_t < t) mx_t = t;
+		if (heights[l] < heights[r]) {
+			int h = heights[l];
+			while (heights[l] <= h) l++;
+		}
+		else if (heights[l] > heights[r]) {
+			int h = heights[r];
+			while (heights[r] <= h) r--;
+		}
+		else {
+			int h = heights[r];
+			while (heights[l] <= h && l < r) l++;
+			while (heights[r] <= h && r > l) r--;
+		}
 	}
 	return mx_t;
 }
