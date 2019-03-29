62596067662b0f0e61a278fa49b919caf66b01b5
Alexey Filippov <afilippov@acm.org>
2019-03-29 08:20:06 +0000

    6.10 SiNuSoIdAl StRiNg

--- epi_judge_cpp/snake_string.cc
+++ epi_judge_cpp/snake_string.cc
@@ -3,13 +3,16 @@
 using std::string;
 
 string SnakeString(const string& s) {
-  // TODO - you fill in here.
-  return "";
+	string res;
+	for (int i = 1; i < s.size(); i += 4) res.push_back(s[i]);
+	for (int i = 0; i < s.size(); i += 2) res.push_back(s[i]);
+	for (int i = 3; i < s.size(); i += 4) res.push_back(s[i]);
+	return res;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "snake_string.cc", "snake_string.tsv",
-                         &SnakeString, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "s" };
+	return GenericTestMain(args, "snake_string.cc", "snake_string.tsv",
+		&SnakeString, DefaultComparator{}, param_names);
 }
