a81661e072e999ddcbdc283e43b3ce85a1c7c0ba
Alexey Filippov <afilippov@acm.org>
2019-05-05 23:04:06 +0100

    7.11 Check for palindromic SL List

--- epi_judge_cpp/is_list_palindromic.cc
+++ epi_judge_cpp/is_list_palindromic.cc
@@ -1,15 +1,58 @@
+#include <utility>
+
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
-bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
-  return true;
+int size(shared_ptr<ListNode<int>> L) {
+    int result = 0;
+    while (L != nullptr) {
+        result++;
+        L = L->next;
+    }
+    return result;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L"};
-  return GenericTestMain(args, "is_list_palindromic.cc",
-                         "is_list_palindromic.tsv", &IsLinkedListAPalindrome,
-                         DefaultComparator{}, param_names);
+shared_ptr<ListNode<int>> reverse(shared_ptr<ListNode<int>> L, shared_ptr<ListNode<int>> tail) {
+    while (L != nullptr) {
+        auto t = L->next;
+        L->next = tail;
+        tail = L;
+        L = t;
+    }
+    return tail;
+}
+
+shared_ptr<ListNode<int>> drop(int n, shared_ptr<ListNode<int>> L) {
+    while (n-- > 0) {
+        L = L->next;
+    }
+    return L;
+}
+
+bool samePrefix(shared_ptr<ListNode<int>> A, shared_ptr<ListNode<int>> B) {
+    while (A != nullptr && B != nullptr) {
+        if (A->data != B->data) return false;
+        A = A->next;
+        B = B->next;
+    }
+    return true;
+}
+
+bool IsLinkedListAPalindrome(const shared_ptr<ListNode<int>> &L) {
+    int n = size(L);
+    if (n <= 1) return true;
+
+    int half = (n + 1) / 2;
+    auto liat = reverse(drop(half, L), nullptr);
+    auto result = samePrefix(liat, L);
+    reverse(liat, nullptr);
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L"};
+    return GenericTestMain(args, "is_list_palindromic.cc",
+                           "is_list_palindromic.tsv", &IsLinkedListAPalindrome,
+                           DefaultComparator{}, param_names);
 }
