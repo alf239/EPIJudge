abc4cee5087671341b65ae9ce179f731b64cda5d
Alexey Filippov <afilippov@acm.org>
2019-05-10 07:29:07 +0100

    8.4 Normalize pathnames

--- epi_judge_cpp/directory_path_normalization.cc
+++ epi_judge_cpp/directory_path_normalization.cc
@@ -1,16 +1,53 @@
 #include <string>
+#include <vector>
 #include "test_framework/generic_test.h"
+
 using std::string;
-string ShortestEquivalentPath(const string& path) {
-  // TODO - you fill in here.
-  return "";
+using std::vector;
+
+void add(vector<string> *parts, const string &s) {
+    if (!s.empty() && s != ".") {
+        if (s == "..") {
+            parts->pop_back();
+        } else {
+            parts->emplace_back(s);
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"path"};
-  return GenericTestMain(args, "directory_path_normalization.cc",
-                         "directory_path_normalization.tsv",
-                         &ShortestEquivalentPath, DefaultComparator{},
-                         param_names);
+string ShortestEquivalentPath(const string &path) {
+    if (path.empty())
+        return path;
+    vector<string> parts;
+    auto a = path.cbegin();
+    for (auto b = path.cbegin(); b != path.cend(); b++) {
+        if (*b == '/') {
+            string s{a, b};
+            add(&parts, s);
+            a = b + 1;
+        }
+    }
+    if (a != path.cend()) {
+        add(&parts, string{a, path.cend()});
+    }
+
+    string result;
+
+    for (auto &part: parts) {
+        result.append("/");
+        result.append(part);
+    }
+
+    return path[0] == '/' ? result : string{result.cbegin() + 1, result.cend()};
+
+}
+
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"path"};
+    return GenericTestMain(args, "directory_path_normalization.cc",
+                           "directory_path_normalization.tsv",
+                           &ShortestEquivalentPath, DefaultComparator{},
+                           param_names);
 }
