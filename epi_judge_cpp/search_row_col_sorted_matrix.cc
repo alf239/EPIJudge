f503ff602912193a42cd217d5bb89ced377394f2
Alexey Filippov <afilippov@acm.org>
2019-06-08 14:46:16 +0100

    11.6 2D search - walk the diagonal

--- epi_judge_cpp/search_row_col_sorted_matrix.cc
+++ epi_judge_cpp/search_row_col_sorted_matrix.cc
@@ -3,9 +3,23 @@
 
 using std::vector;
 
-bool MatrixSearch(const vector<vector<int>> &A, int x) {
-    // TODO - you fill in here.
+bool search(const vector<vector<int>> &A, int x, int top, int bottom, int left, int right) {
+    if (top > bottom) return false;
+    if (left > right) return false;
+    
+    const int tr = A[top][right];
+    
+    if (x > tr) return search(A, x, top + 1, bottom, left, right);
+    if (x < tr) return search(A, x, top, bottom, left, right - 1);
     return true;
+}
+
+bool MatrixSearch(const vector<vector<int>> &A, int x) {
+    if (A.empty()) return false;
+    if (A[0].empty()) return false;
+    auto N = A.size(), M = A[0].size();
+
+    return search(A, x, 0, N - 1, 0, M - 1);
 }
 
 int main(int argc, char *argv[]) {
