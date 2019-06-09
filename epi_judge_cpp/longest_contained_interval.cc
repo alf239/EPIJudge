9c8bad58ebf764088ea8a760951007166a8ea4fb
Alexey Filippov <afilippov@acm.org>
2019-06-09 11:53:33 +0100

    12.9 Max interval as per book. Well...

--- epi_judge_cpp/longest_contained_interval.cc
+++ epi_judge_cpp/longest_contained_interval.cc
@@ -1,11 +1,19 @@
-#include <vector>
+#include <set>
 #include "test_framework/generic_test.h"
 
 using std::vector;
 
 int LongestContainedRange(const vector<int> &A) {
-    // TODO - you fill in here.
-    return 0;
+    std::unordered_set<int> seen;
+    int maxLen = 0;
+    for (const auto a : A) seen.emplace(a);
+    for (const auto a : A) {
+        int count = 1;
+        for (int aa = a - 1; seen.erase(aa) > 0; aa--) count++;
+        for (int aa = a + 1; seen.erase(aa) > 0; aa++) count++;
+        if (count > maxLen) maxLen = count;
+    }
+    return maxLen;
 }
 
 int main(int argc, char *argv[]) {
