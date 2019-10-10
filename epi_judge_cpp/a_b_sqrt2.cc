3bf2acc36d10c797cf2bf261df03811bc04e192d
Alexey Filippov <afilippov@acm.org>
2019-10-10 22:57:25 +0100

    14.7 a + b*sqrt(2) - with set

--- epi_judge_cpp/a_b_sqrt2.cc
+++ epi_judge_cpp/a_b_sqrt2.cc
@@ -1,24 +1,31 @@
 #include <vector>
 #include "test_framework/generic_test.h"
-#include <queue>
+#include <set>
 
 using std::vector;
-using std::priority_queue;
+using std::set;
 using std::greater;
 
+struct Number {
+    Number(int a, int b) : a(a), b(b), value(a + b * sqrt(2)) {}
+
+    int a, b;
+    double value;
+};
+
 vector<double> GenerateFirstKABSqrt2(int k) {
-	auto root = sqrt(2);
-	priority_queue<double, vector<double>, greater<double>> work;
-    for (int a = 0; a <= k; a++) {
-    	auto lim = (k - a) / root;
-    	for (int b = 0; b <= lim; b++) {
-    		work.push(a + b * root);
-    	}
-    }
+    auto root = sqrt(2);
+    auto cmp = [](Number left, Number right) { return left.value < right.value; };
+    set<Number, decltype(cmp)> work(cmp);
+    work.emplace(0, 0);
+
     vector<double> result;
     for (int i = 0; i < k; i++) {
-    	result.push_back(work.top());
-    	work.pop();
+        auto min = work.cbegin();
+        work.emplace(min->a + 1, min->b);
+        work.emplace(min->a, min->b + 1);
+        work.erase(min);
+        result.push_back(min->value);
     }
     return { result.cbegin(), result.cend() };
 }
