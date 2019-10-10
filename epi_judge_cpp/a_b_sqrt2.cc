5d64a004a78a2968e2f3f4c27589d58e39b551b9
Alexey Filippov <afilippov@acm.org>
2019-10-10 22:34:09 +0100

    14.7 a + b*sqrt(2)

--- epi_judge_cpp/a_b_sqrt2.cc
+++ epi_judge_cpp/a_b_sqrt2.cc
@@ -1,11 +1,26 @@
 #include <vector>
 #include "test_framework/generic_test.h"
+#include <queue>
 
 using std::vector;
+using std::priority_queue;
+using std::greater;
 
 vector<double> GenerateFirstKABSqrt2(int k) {
-    // TODO - you fill in here.
-    return {};
+	auto root = sqrt(2);
+	priority_queue<double, vector<double>, greater<double>> work;
+    for (int a = 0; a <= k; a++) {
+    	auto lim = (k - a) / root;
+    	for (int b = 0; b <= lim; b++) {
+    		work.push(a + b * root);
+    	}
+    }
+    vector<double> result;
+    for (int i = 0; i < k; i++) {
+    	result.push_back(work.top());
+    	work.pop();
+    }
+    return { result.cbegin(), result.cend() };
 }
 
 int main(int argc, char *argv[]) {
