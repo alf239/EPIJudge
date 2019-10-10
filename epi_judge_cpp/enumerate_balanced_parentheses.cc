f974f74cc0bb59e850d468054420395c3ebbbc94
Alexey Filippov <afilippov@acm.org>
2019-10-10 23:35:36 +0100

    15.7 Enumerate balanced parentheses

--- epi_judge_cpp/enumerate_balanced_parentheses.cc
+++ epi_judge_cpp/enumerate_balanced_parentheses.cc
@@ -1,13 +1,33 @@
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include "test_framework/generic_test.h"
 
 using std::string;
 using std::vector;
+using std::unordered_set;
 
 vector<string> GenerateBalancedParentheses(int num_pairs) {
-    // TODO - you fill in here.
-    return {};
+    if (num_pairs == 0) return { "" };
+    vector<vector<string>> partial;
+    for (int i = 0; i < num_pairs; i++) {
+        partial.emplace_back(GenerateBalancedParentheses(i));
+    }
+    unordered_set<string> result;
+    for (int i = 1; i < num_pairs; i++) {
+        for (auto s1 : partial[i]) {
+            for (auto s2 : partial[num_pairs - i]) {
+                result.emplace(s1 + s2);
+                if (i != num_pairs - i) {
+                    result.emplace(s2 + s1);
+                }
+            }
+        }
+    }
+    for (auto s : partial[num_pairs - 1]) {
+        result.emplace("(" + s + ")");
+    }
+    return { result.cbegin(), result.cend() };
 }
 
 int main(int argc, char *argv[]) {
