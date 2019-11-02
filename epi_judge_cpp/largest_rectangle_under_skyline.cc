5cda8bdc1fd4f65e4e2b5b39b8ad31d2dfcbbad2
Alexey Filippov <afilippov@acm.org>
2019-11-02 14:50:40 +0000

    17.8 Largest rectangle under the skyline, O(n^2)

--- epi_judge_cpp/largest_rectangle_under_skyline.cc
+++ epi_judge_cpp/largest_rectangle_under_skyline.cc
@@ -3,16 +3,28 @@
 
 using std::vector;
 
-int CalculateLargestRectangle(const vector<int> &heights) {
-    // TODO - you fill in here.
-    return 0;
+int CalculateLargestRectangle(const vector < int > &heights) {
+	int hprev = 0, size = 0;
+	for (int i = 0; i < heights.size(); i++) {
+		for (int h = heights[i]; h > hprev; h--) { 
+			int w = 0; 
+			while (i + w < heights.size() && heights[i + w] >= h) {
+				w++;
+			}
+			if (size < h * w) {
+				size = h * w;
+			}
+		}
+		hprev = heights[i];
+	}
+	return size;
 }
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> args{argv + 1, argv + argc};
-    std::vector<std::string> param_names{"heights"};
-    return GenericTestMain(args, "largest_rectangle_under_skyline.cc",
-                           "largest_rectangle_under_skyline.tsv",
-                           &CalculateLargestRectangle, DefaultComparator{},
-                           param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "heights" };
+	return GenericTestMain(args, "largest_rectangle_under_skyline.cc",
+		"largest_rectangle_under_skyline.tsv",
+		&CalculateLargestRectangle, DefaultComparator{},
+		param_names);
 }
